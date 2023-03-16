08c1c9822c920244c63c0d6ddb341345d79cace0
Eliana-2 <eliana.guizar1@gmail.com>
2023-03-15 18:10:23 -0700

    Add CalculatorFactory function and tests

--- modules/tests.test.js
+++ modules/tests.test.js
@@ -1,26 +1,43 @@
 import {test, expect} from 'vitest';
 import {capitalize, reverseString} from './stringManipulation.js';
+import {CalculatorFactory} from './calculator.js';
 
 function capitalizeTests() {
-  test('First character capitalized', () => {
+  test('capitalize: First character capitalized', () => {
     expect(capitalize('moo')).toBe('Moo');
   });
-  test('Nonletter first', () => {
+  test('capitalize: Nonletter first', () => {
     expect(capitalize('2moo4u')).toBe('2moo4u');
   });
-  test('Empty string', () => {
+  test('capitalize: Empty string', () => {
     expect(capitalize('')).toBe('');
   });
 }
 
 function reverseStringTests() {
-  test('String reversed', () => {
+  test('reverseString: String reversed', () => {
     expect(reverseString('moo')).toBe('oom');
   });
-  test('Empty string', () => {
+  test('reverseString: Empty string', () => {
     expect(reverseString('')).toBe('');
+  });
+}
+
+function CalculatorFactoryTests() {
+  test('CalculatorFactory: Add 2 numbers', () => {
+    expect(CalculatorFactory().add(2, 13)).toBe(15);
+  });
+  test('CalculatorFactory: Subtract 2 numbers', () => {
+    expect(CalculatorFactory().subtract(5, 3)).toBe(2);
+  });
+  test('CalculatorFactory: Multiply 2 numbers', () => {
+    expect(CalculatorFactory().multiply(3, 7)).toBe(21);
+  });
+  test('CalculatorFactory: Divide 2 numbers', () => {
+    expect(CalculatorFactory().divide(10, 5)).toBe(2);
   });
 }
 
 capitalizeTests();
 reverseStringTests();
+CalculatorFactoryTests();
